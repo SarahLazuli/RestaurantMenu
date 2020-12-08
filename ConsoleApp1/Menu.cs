4e37a94a790222211f0d3c2c067991c1a6aa58a6
valboaten@gmail.com <valboaten@gmail.com>
2020-12-07 20:49:27 -0600

    Class Code

---
+++ ConsoleApp1/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        private List<MenuItems> menuList;
+        public String RestaurantName { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+       public void  PrintMenu ()
+        {
+            //loop through menulist
+            //compare Date Added > Last Update print new next to it
+        }
+
+        public void AddItem(MenuItems item)
+        {
+            menuList.Add(item);
+        }
+    }
+}
---
+++ ConsoleApp1/MenuItems.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItems
+    {
+        private Decimal _price;
+        //private String _description;
+        //private String _category;
+        //private DateTime _dateAdded;
+        //private Boolean _isNew;
+
+        public Decimal Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                _price = value;
+            }
+        }
+
+        public String Description { get; set; }
+        public String Category { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public Boolean IsNew { get; set; }
+
+        public MenuItems (String description, Decimal price, String category, DateTime dateAdded, Boolean isNew)
+        {
+            Description = description;
+            Price = price;
+            Category = category;
+            DateAdded = dateAdded;
+            IsNew = isNew;
+
+        }
+        public MenuItems(String description, Decimal price, String category)
+        {
+            Description = description;
+            Price = price;
+            Category = category;
+            DateAdded = DateTime.Today;
+            IsNew = false;
+
+        }
+
+        
+
+ 
+
+    }
+}
